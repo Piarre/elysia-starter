4ddc8ba58e8d0e511bb928ae82fe957321000bf6
Kepa <pierre@ideestore.fr>
2025-10-21 13:09:58 +0200

    chore(debug route): refactor uptime and healthcheck routes

--- src/routes/debug.ts
+++ src/routes/debug.ts
@@ -3,8 +3,6 @@ import { uptime } from "../utils/uptime";
 
 export default new Elysia().group("/debug", (route) =>
   route
-    .get("/uptime", () => uptime())
-    .get("/healthcheck", () => {
-      return { status: "ok", uptime: uptime() };
-    }),
+    .get("/uptime", uptime())
+    .get("/healthcheck", { status: "ok", uptime: uptime() }),
 );
